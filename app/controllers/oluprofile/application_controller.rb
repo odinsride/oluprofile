711a879dd1125ab2b65dd6c016e9d153bc4758a1
Kevin Custer <kecuster@gmail.com>
2018-10-02 08:26:01 -0400

    refactor navbar a bit

--- app/controllers/oluprofile/application_controller.rb
+++ app/controllers/oluprofile/application_controller.rb
@@ -3,6 +3,12 @@ module Oluprofile
     before_action :require_login
     protect_from_forgery with: :exception
 
+    def rails_env
+      rails_env = !Rails.env.production? ? Rails.env.upcase : nil
+    end
+
+    helper_method :rails_env
+
     private
 
       def not_authenticated
